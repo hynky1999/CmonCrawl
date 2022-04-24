3b3f40a68af660a2eac88714f9b5fd4f17ee6798
Hynek Kydlicek <kydlicek.hynek@gmail.com>
2022-04-24 22:55:38 +0200

    Pre-merge

--- Aggregator/main.py
+++ Aggregator/main.py
@@ -2,11 +2,6 @@ from index_query import DomainIndexer, Domain_Record
 from reddis_helpers import create_pool
 import aioredis
 
-async def process_and_forward(record: Domain_Record, redis_conn:aioredis.Redis ) -> None:
-    redis_conn.get
-
-    if redis_conn.setnx(record.filename)
-
 
 
 async def main():
