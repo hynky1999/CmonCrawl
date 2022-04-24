c0313709b980a2b9f62a6793660f880b48d2d8eb
Hynek Kydlicek <kydlicek.hynek@gmail.com>
2022-04-24 22:56:08 +0200

    pre-merge

---
+++ Aggregator/reddis_helpers.py
@@ -0,0 +1,26 @@
+from ctypes import Union
+from datetime import datetime
+import aioredis
+import asyncio
+imoprt datetime
+from index_query import Domain_Record
+
+
+async def process_and_forward(
+    redis_conn: aioredis.Redis, record: Domain_Record
+) -> int | None:
+    # Already set
+    if redis_conn.get(record.url) is not None:
+        return None
+    
+    # Try set if not already set
+    cur_time = int(time.time())
+    if redis_conn.setnx(record.url, cur_time) is None:
+        return None
+
+    return cur_time
+
+
+
+def create_connection(url: str):
+    return aioredis.from_url(url)
