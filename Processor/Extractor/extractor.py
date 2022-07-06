835dda5579ee3a1c2dc8900de90f832a2e7780b9
Hynek Kydlicek <kydlicek.hynek@gmail.com>
2022-07-06 04:18:02 +0200

    Added logging and Error handlings

--- Processor/Extractor/extractor.py
+++ Processor/Extractor/extractor.py
@@ -1,4 +1,5 @@
 from abc import ABC, abstractmethod
+import logging
 from typing import Any, Dict
 from utils import PipeMetadata
 
@@ -13,10 +14,14 @@ class BaseExtractor(ABC):
 
     def extract(self, response: str, metadata: PipeMetadata) -> Dict[Any, Any] | None:
         if self.filter(response, metadata) is False:
+            logging.info(f"{metadata.domain_record.url}: Filter failed")
             return None
 
         article = self.preprocess(response, metadata)
-        return self.extract_no_preprocess(article, metadata)
+        result = self.extract_no_preprocess(article, metadata)
+        if result is None:
+            logging.info(f"{metadata.domain_record.url}: Extract failed")
+        return result
 
     @abstractmethod
     def extract_no_preprocess(
--- Processor/Pipeline/pipeline.py
+++ Processor/Pipeline/pipeline.py
@@ -15,16 +15,15 @@ class ProcessorPipeline:
 
     async def process_domain_record(self, domain_record: DomainRecord):
         metadata = PipeMetadata(domain_record=domain_record)
-        downloaded_article = await self.downloader.download(domain_record, metadata)
-        # This will not be None
-        extractor = self.router.route(metadata.domain_record.url)
+        try:
+            downloaded_article = await self.downloader.download(domain_record, metadata)
+            extractor = self.router.route(metadata.domain_record.url)
+        except ValueError as e:
+            logging.error(f"{domain_record.url}: {e}")
+            return None
+
         output = extractor.extract(downloaded_article, metadata)
         if output is None:
-            print(f" Failed to download {metadata.domain_record.url}")
             return None
         path = await self.oustreamer.stream(output, metadata)
-
-        logging.debug(
-            f"Successfully downloaded article from {domain_record.url} at {path}"
-        )
         return path
