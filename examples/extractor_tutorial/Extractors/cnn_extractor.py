e344adbb47b0167f1ef0d4c7e9d5bcebd9050591
Hynek Kydlíček <hynek.kydlicek@blindspot.ai>
2023-09-09 02:02:45 +0200

    🔧 examples

---
+++ examples/extractor_tutorial/Extractors/cnn_extractor.py
@@ -0,0 +1,27 @@
+from typing import Any, Dict
+from bs4 import BeautifulSoup
+from cmoncrawl.common.types import PipeMetadata
+from cmoncrawl.processor.pipeline.extractor import BaseExtractor
+
+
+class CNNExtractor(BaseExtractor):
+    def __init__(self):
+        super().__init__()
+
+    def extract_soup(
+        self, soup: BeautifulSoup, metadata: PipeMetadata
+    ) -> Dict[str, Any] | None:
+        maybe_title = soup.select_one("div.headline > h1")
+        maybe_content = soup.select_one("#posts")
+        if maybe_title is None or maybe_content is None:
+            return None
+
+        title = maybe_title.text
+        content = maybe_content.text
+        return {
+            "title": title,
+            "content": content,
+        }
+
+
+page_extractor = CNNExtractor()
