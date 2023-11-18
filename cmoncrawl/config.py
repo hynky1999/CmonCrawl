c710a695f75faf2b2027437b549eb3741b98fbb2
Hynek Kydlíček <hynek.kydlicek@blindspot.ai>
2023-11-18 14:17:35 +0100

    ci fix

--- cmoncrawl/config.py
+++ cmoncrawl/config.py
@@ -7,9 +7,17 @@ from dotenv import load_dotenv
 load_dotenv()
 
 
+def get_str_env(name: str, default: str | None) -> str | None:
+    env = os.getenv(name, default)
+    if not env:
+        return None
+
+    return env
+
+
 @dataclass
 class Config:
-    AWS_PROFILE: str | None = os.getenv("AWS_PROFILE")
+    AWS_PROFILE: str | None = get_str_env("AWS_PROFILE", None)
 
     def update_from_cli(self, args: argparse.Namespace):
         self.AWS_PROFILE = args.aws_profile or self.AWS_PROFILE  # type: ignore
